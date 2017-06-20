880a72afb3b3ba530b95e26d5cac96834ebfbc63
Emmanuelle <emma.carre@gmail.com>
2017-06-20 10:29:17 +0200

    commit

---
+++ faire_play_mobil/src/AppBundle/Controller/ProjectController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Project;
+use AppBundle\Form\ProjectType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
+class ProjectController extends Controller
+{
+    /**
+     * @Route ("/administration/gestion_projets", name="gestion_projets")
+     */
+    public function newAction(Request $request)
+    {
+        $project = new Project();
+
+        $form = $this->createForm(ProjectType::class, $project);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            if ($form->isValid()) {
+
+                $photo = $project->getPhoto();
+                $photo->move(
+                    $this->getParameter('img_directory'),
+                    uniqid().$photo->getClientOriginalName()
+                );
+
+                //$project = $form->getData();
+                $project->setPhoto( $photo->getClientOriginalName());
+
+                $em->persist($project);
+                $em->flush();
+                return new Response('Le projet est validé !');
+            }
+        }
+
+
+        return $this->render(':Admin:gestion_projets.html.twig', array('form' => $form->createView()));
+    }
+
+}
