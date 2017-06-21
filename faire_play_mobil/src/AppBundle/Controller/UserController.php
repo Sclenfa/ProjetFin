6af94696698bf4f1c0a27b13f8b94b7d9bf8a0cc
NourRakotoson <nour.rakotoson@gmail.com>
2017-06-21 14:48:48 +0200

    Mise à jour gros 2

---
+++ faire_play_mobil/src/AppBundle/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Etudiant
+ * Date: 21/06/2017
+ * Time: 10:44
+ */
+
+namespace AppBundle\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class UserController extends Controller
+{
+
+    /**
+     * @route("/users", name="/users")
+     */
+    public function showUserAction(EntityManagerInterface $em)
+    {
+        $users = $em->getRepository('AppBundle:User')
+            ->findAll();
+
+            return $this->render('users.html.twig', ['users' => $users]);
+
+    }
+
+}
